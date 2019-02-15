1df89f443ceecef2d2de3e657f26a52cebdc4629
Arvind S <arvind0598@gmail.com>
2019-02-15 23:55:17 +0530

    now fetches attendance

--- parser.rb
+++ parser.rb
@@ -12,14 +12,38 @@ class Parser
       course_data = {}
       course_data['name'] = course[1].text.split.map(&:capitalize).join(' ')
       course_data['semester'] = course[4].text
-      course_data['credits'] = course[5].text
-      course_map[course[0].text] = course_data
+      course_data['credits'] = course[5].text.to_f
+      course_map[course[0].text.split.join] = course_data
     }
     return course_map
   end
 
+  def self.get_attendance(html)
+    data = Nokogiri::HTML(html)
+    attendance_details = data.css('#tblAttendancePercentage td')
+
+    attendance_map = {}
+
+    attendance_details.each_slice(8) { |course|
+      subject_code = course[1].text.split.join
+      attendance_total = course[4].text
+      attendance_present = course[5].text
+      course_data = {
+        total: attendance_total.to_i,
+        present: attendance_present.to_i,
+      }
+      attendance_map[subject_code] = course_data
+    }
+    return attendance_map
+  end
+
   def self.get_academics_details(html)
-    courses = get_courses(html).to_json
-    return courses
+    courses = get_courses(html)
+    attendance = get_attendance(html)
+    details = {
+      courses: courses,
+      attendance: attendance,
+    }
+    return details.to_json
   end
 end
