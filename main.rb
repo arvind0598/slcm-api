8a54dba4d7943127c6ac927cccac28cd384cdf8d
Arvind S <arvind0598@gmail.com>
2019-02-17 03:20:18 +0530

    added route for gradesheet

--- main.rb
+++ main.rb
@@ -1,9 +1,8 @@
 require 'sinatra'
 require 'json'
 
-require_relative 'script.rb'
+require_relative 'requests.rb'
 require_relative 'parser.rb'
-require_relative 'utils.rb'
 
 # Sample route
 get '/' do
@@ -14,7 +13,7 @@ end
 post '/login' do
   content_type :json
   data = JSON.parse(request.body.read)
-  username, password, error = check_credentials(data)
+  username, password, error = Utils.check_credentials(data)
   
   unless error.nil? 
     return error 
@@ -28,7 +27,7 @@ end
 post '/bunks' do
   content_type :json
   data = JSON.parse(request.body.read)
-  username, password, error = check_credentials(data)
+  username, password, error = Utils.check_credentials(data)
 
   unless error.nil? 
     return error 
@@ -41,6 +40,65 @@ post '/bunks' do
   end
 
   html = SLCM.get_academics_page(session)
-  attendance_details = Parser.get_attendance(html)
-  { success: true, data: attendance_details }.to_json
-end 
+  details = Parser.get_attendance(html)
+  { success: true, data: details }.to_json
+end
+
+# Route to fetch course details
+post '/courses' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+  username, password, error = Utils.check_credentials(data)
+
+  unless error.nil? 
+    return error 
+  end
+
+  session = SLCM.get_session_cookie()
+  login_status = SLCM.login_user(username, password, session)
+  unless login_status[:success]
+    return login_status.to_json
+  end
+
+  html = SLCM.get_academics_page(session)
+  details = Parser.get_courses(html)
+  { success: true, data: details }.to_json
+end
+
+# Route to fetch gradesheet details
+post '/grades' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+  username, password, error = Utils.check_credentials(data)
+
+  semester = data['semester']
+  if semester.nil?
+    return Utils.send_status(false, 'Semester is missing').to_json
+  else
+    conv_status = Utils.map_roman_integer(semester, :int_to_roman)
+    unless conv_status[:success]
+      return Utils.send_status(false, 'Semester is invalid').to_json
+    end
+    semester = conv_status[:message]
+  end
+
+  unless error.nil? 
+    return error 
+  end
+
+  session = SLCM.get_session_cookie()
+  login_status = SLCM.login_user(username, password, session)
+  unless login_status[:success]
+    return login_status.to_json
+  end
+
+  # TODO: split auth data into own route
+  gradesheet_html = SLCM.get_gradesheet_page(session)
+  viewstate, eventvalidation = Parser.get_grades_auth(gradesheet_html)
+  puts viewstate, eventvalidation
+
+  html = SLCM.get_gradesheet_info(session, viewstate, eventvalidation, semester)
+  details = Parser.get_grades(html)
+  { success: true, data: details }.to_json
+end
+
--- parser.rb
+++ parser.rb
@@ -1,6 +1,18 @@
 require 'nokogiri'
 
 class Parser
+  def self.get_course_name(name)
+    # name.split.map{|x| 
+    #   if x.length > 3 || x == 'LAB'
+    #     x.capitalize
+    #   elsif Utils.map_roman_integer(x, :roman_to_int)[:success]
+    #     x.upcase
+    #   else
+    #     x.downcase
+    #   end
+    # }.join(' ')
+    name.strip
+  end
 
   def self.get_courses(html)
     data = Nokogiri::HTML(html)
@@ -9,11 +21,11 @@ class Parser
     course_map = {}
 
     course_details.each_slice(9) { |course|
-      course_data = {}
-      course_data['name'] = course[1].text.split.map(&:capitalize).join(' ')
-      course_data['semester'] = course[4].text
-      course_data['credits'] = course[5].text.to_f
-      course_map[course[0].text.split.join] = course_data
+      subject_code = course[0].text.split.join
+      name = get_course_name(course[1].text)
+      semester = course[4].text
+      credits = course[5].text.to_f
+      course_map[subject_code] = { name: name, semester: semester, credits: credits }
     }
     return course_map
   end
@@ -35,5 +47,36 @@ class Parser
       attendance_map[subject_code] = course_data
     }
     return attendance_map
+  end
+
+  def self.get_grades_auth(html)
+    data = Nokogiri::HTML(html)
+    # puts data
+    viewstate = data.at('input[name="__VIEWSTATE"]')['value']
+    eventvalidation = data.at('input[name="__EVENTVALIDATION"]')['value']
+    p viewstate, eventvalidation
+    return viewstate, eventvalidation
+  end
+
+  def self.get_grades(html)
+    data = Nokogiri::HTML(html)
+    grades_details = data.css('#ContentPlaceHolder1_grvGradeSheet td')
+
+    gpa = data.css('#ContentPlaceHolder1_lblGPA').text.to_f
+    cgpa = data.css('#ContentPlaceHolder1_lblCGPA').text.to_f
+    total_credits = data.css('#ContentPlaceHolder1_LabelTotalcredit').text.to_f
+
+    grades_map = {}
+
+    grades_details.each_slice(8) { |course|
+      subject_code = course[1].text.split.join
+      name = get_course_name(course[2].text)
+      grade = course[3].text.strip
+      credits = course[4].text.to_f
+      grades_map[subject_code] = { name: name, grade: grade, credits: credits }     
+    }
+
+    details = { cgpa: cgpa, gpa: gpa, credits: total_credits, grades: grades_map }
+    return details
   end
 end
