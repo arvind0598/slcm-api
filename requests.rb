82db293b17980d90d2093f124a0fbf36c463e6d9
Arvind S <arvind0598@gmail.com>
2019-02-17 03:19:37 +0530

    moved send status to utils and added gradesheet requests

--- requests.rb
+++ requests.rb
@@ -4,19 +4,12 @@ require 'json'
 require 'pry'
 
 require_relative 'slcm.rb'
+require_relative 'utils.rb'
 
 $slcm = Site.new
 $timeout = 20
 
 class SLCM
-
-  def self.send_status(status, message = nil)
-    {
-      success: status,
-      message: message,
-    }
-  end
-
   def self.get_session_cookie
     response = HTTParty.get(
       $slcm.get_login_url(), 
@@ -38,9 +31,9 @@ class SLCM
     )
     status = response.length < 100
     if status 
-      return send_status(true, 'Login Successful!')
+      return Utils.send_status(true, 'Login Successful!')
     else
-      return send_status(false, 'Invalid Credentials.')
+      return Utils.send_status(false, 'Invalid Credentials.')
     end
   end
   
@@ -50,6 +43,28 @@ class SLCM
       $slcm.get_academics_url(),
       timeout: $timeout,
       headers: get_headers,
+    )
+  end
+
+  def self.get_gradesheet_page(session)
+    get_headers = $slcm.make_get_headers(session)
+    response = HTTParty.get(
+      $slcm.get_grades_url(),
+      timeout: $timeout,
+      headers: get_headers,
+    )
+  end
+
+  def self.get_gradesheet_info(session, viewstate, eventvalidation, semester)
+    post_body = $slcm.make_gradesheet_body(semester, viewstate, eventvalidation)
+    puts post_body
+    post_headers = $slcm.make_post_headers(post_body.to_json.length.to_s, session)
+    post_headers['Referer'] = $slcm.get_grades_url().to_s
+    response = HTTParty.post(
+      $slcm.get_grades_url(),
+      timeout: $timeout,
+      headers: post_headers,
+      body: post_body,
     )
   end
 end
