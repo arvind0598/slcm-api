101dbc1f389bd4c2895bc8c43216f2d41afa7ddd
Arvind S <arvind0598@gmail.com>
2019-02-17 03:18:33 +0530

    moved send status and added roman map

--- utils.rb
+++ utils.rb
@@ -1,9 +1,30 @@
-def check_credentials(data)
-  username = data['username']
-  password = data['password']
-  error = nil
-  if username.nil? || password.nil? || !username.match(/^(\d){9}$/)
-    error = { success: false, message: 'Invalid Credentials.'}.to_json
+class Utils
+  def self.send_status(status, message = nil)
+    { success: status, message: message }
   end
-  return username, password, error
-end
+  
+  def self.check_credentials(data)
+    username = data['username']
+    password = data['password']
+    error = nil
+    if username.nil? || password.nil? || !username.match(/^(\d){9}$/)
+      error = { success: false, message: 'Invalid Credentials.'}.to_json
+    end
+    return username, password, error
+  end
+  
+  def self.map_roman_integer(num, direction)
+    roman_nums = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII']
+    status = nil
+    begin
+      if direction == :roman_to_int
+        status = roman_nums.find_index(num)
+      else
+        status = roman_nums.at(num.to_i)
+      end
+    rescue
+      return send_status(false, 'An error occured!')
+    end
+    return send_status(!status.nil?, status)
+  end
+end  
