4290ff8df6022e24056bed3fe3493ab01c90cd89
Arvind S <arvind0598@gmail.com>
2019-03-24 00:53:34 +0530

    changed responses to arrays for easier app dev ease

--- parser.rb
+++ parser.rb
@@ -1,31 +1,24 @@
 require 'nokogiri'
 
 class Parser
-  def self.get_course_name(name)
-    # name.split.map{|x| 
-    #   if x.length > 3 || x == 'LAB'
-    #     x.capitalize
-    #   elsif Utils.map_roman_integer(x, :roman_to_int)[:success]
-    #     x.upcase
-    #   else
-    #     x.downcase
-    #   end
-    # }.join(' ')
-    name.strip
-  end
 
   def self.get_courses(html)
     data = Nokogiri::HTML(html)
     course_details = data.css('.row + .table-responsive.mt-10 .table.table-bordered td')
 
-    course_map = {}
+    course_map = []
 
     course_details.each_slice(9) { |course|
       subject_code = course[0].text.split.join
-      name = get_course_name(course[1].text)
+      name = course[1].text.strip
       semester = Utils.map_roman_integer(course[4].text, :roman_to_int)[:message]
       credits = course[5].text.to_f
-      course_map[subject_code] = { name: name, semester: semester, credits: credits }
+      course_map.push({ 
+        code: subject_code,
+        name: name, 
+        semester: semester, 
+        credits: credits 
+      })
     }
     return course_map
   end
@@ -34,17 +27,18 @@ class Parser
     data = Nokogiri::HTML(html)
     attendance_details = data.css('#tblAttendancePercentage td')
 
-    attendance_map = {}
+    attendance_map = []
 
     attendance_details.each_slice(8) { |course|
       subject_code = course[1].text.split.join
       attendance_total = course[4].text
       attendance_present = course[5].text
       course_data = {
+        code: subject_code,
         total: attendance_total.to_i,
         present: attendance_present.to_i,
       }
-      attendance_map[subject_code] = course_data
+      attendance_map.push(course_data)
     }
     return attendance_map
   end
@@ -64,14 +58,19 @@ class Parser
     cgpa = data.css('#ContentPlaceHolder1_lblCGPA').text.to_f
     total_credits = data.css('#ContentPlaceHolder1_LabelTotalcredit').text.to_f
 
-    grades_map = {}
+    grades_map = []
 
     grades_details.each_slice(8) { |course|
       subject_code = course[1].text.split.join
-      name = get_course_name(course[2].text)
+      name = course[2].text.strip
       grade = course[3].text.strip
       credits = course[4].text.to_f
-      grades_map[subject_code] = { name: name, grade: grade, credits: credits }     
+      grades_map.push(course_data = {
+        code: subject_code,
+        name: name, 
+        grade: grade, 
+        credits: credits
+      })
     }
 
     details = { cgpa: cgpa, gpa: gpa, credits: total_credits, grades: grades_map }
@@ -82,29 +81,39 @@ class Parser
     data = Nokogiri::HTML(html)
     marks_details = data.css('.panel-collapse.collapse')
 
-    marks_map = {}
+    marks_array = []
 
     marks_details.each{ |x|
       subject_code = x['id']
+      total_marks = {}
       subject_marks_details = x.css('td')
-      subject_map = {}
+      subject_marks_array = []
 
       subject_marks_details.each_slice(3) { |test|
         name = test[0].text
-        if name.include?'Total'
-          next
-        end
-
         total = test[1].text.to_f
         score = test[2].text.to_f
-        subject_marks = { score: score, total: total }
-        subject_map[name] = subject_marks
+        subject_marks = {
+          score: score, 
+          total: total
+        }
+
+        if name.include?'Total'
+          total_marks = subject_marks
+        else
+          subject_marks['name'] = name
+          subject_marks_array.push(subject_marks)
+        end
       }
 
-      marks_map[subject_code] = subject_map
+      marks_array.push({
+        code: subject_code,
+        total: total_marks,
+        marks: subject_marks_array
+      })
     }
 
-    marks_map
+    return marks_array
   end
 
   def self.get_profile(html)
